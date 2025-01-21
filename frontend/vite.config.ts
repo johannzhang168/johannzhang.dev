a13afc84f84a6410032f84298b5995a5aef72abe
johannzhang8 <johann.zhang@tufts.edu>
2025-01-20 20:33:03 -0500

    update vite config because deployment does not route pages correctly

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import * as path from "path";
+import * as path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -9,5 +9,13 @@ export default defineConfig({
       "@app": path.resolve(__dirname, "./app"),
     },
   },
-  root: '.',
+  root: ".", 
+  build: {
+    outDir: "dist", 
+    emptyOutDir: true, 
+  },
+  server: {
+    port: 5173, 
+    open: true, 
+  },
 });
