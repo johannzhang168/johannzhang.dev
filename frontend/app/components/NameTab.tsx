0ced96439d9502e4dded73ec66b2036b1a56efa5
johannzhang8 <johann.zhang@tufts.edu>
2025-01-22 01:15:33 -0500

    more small changes

--- frontend/app/components/NameTab.tsx
+++ frontend/app/components/NameTab.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NameTab: React.FC = () => {
   return (
-    <div className="bg-white rounded-lg shadow-md px-[1vw] py-[2vh] flex w-[50vw] h-[100px]">
+    <div className="bg-white rounded-lg shadow-md px-[1vw] py-[10px] flex w-[50vw] h-[100px]">
       <div className="text-left">
         <a href="/" className="text-4xl font-semibold hover:underline">Johann Zhang</a>
         <p className="text-3xl">张钰檀</p>
