2414fabb47dd1732e242446f7aecd4cfabf6dfc2
johannzhang8 <johann.zhang@tufts.edu>
2025-02-04 17:46:26 -0500

    i suck at frontend development

--- frontend/app/components/NameTab.tsx
+++ frontend/app/components/NameTab.tsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const NameTab: React.FC = () => {
   return (
-    <div className="bg-white rounded-lg shadow-md px-[1vw] py-[10px] flex w-[50vw] h-[75px] max-w-[750px] sm:h-[100px] md:h-[100px] lg:h-[100px] overflow-hidden">
+    <div className="bg-white rounded-lg shadow-md px-[1vw] py-[10px] flex w-[75vw] sm:md:lg:w-[50vw] max-w-[750px] h-[100px] overflow-hidden">
       <div className="text-left">
         <a
           href="/"
-          className="text-lg sm:text-4xl md:text-4xl lg:text-4xl font-semibold hover:underline truncate"
+          className=" text-3xl sm:md:lg:text-4xl font-semibold hover:underline truncate"
         >
           Johann Zhang
         </a>
-        <p className="text-md sm:text-3xl md:text-3xl lg:text-3xl truncate">
+        <p className="text-2xl sm:md:lg:text-3xl truncate">
           张钰檀
         </p>
       </div>
